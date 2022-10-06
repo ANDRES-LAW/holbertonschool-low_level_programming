37469a44f178c36f7af1bebb32961e71402025c0
ANDRES-LAW <lawilder.03@gmail.com>
2022-10-06 12:11:56 -0700

    s

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,11 @@
-nclude <stdio.h>
-
-
+/**
+ * main - prints required sentence
+ * Return: 0
+ */
+#include <stdio.h>
 
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
