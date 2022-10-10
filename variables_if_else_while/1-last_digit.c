5b8c2ce0ff57d78bd013dedaaf6d223b3f0297d5
ANDRES-LAW <lawilder.03@gmail.com>
2022-10-10 11:14:33 -0700

    correccion 1-1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,13 +6,12 @@
  * main - Random number + last digit
  * Return: 0
  */
-
 int main(void)
 
 {
 	int n;
 	int l;
-			
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
@@ -20,10 +19,11 @@ int main(void)
 	if (l > 5)
 	{												        printf("and is greater than 5\n");
 	}
-	else if (l == 0) 
-	{												        printf("and is 0\n");								        }
- 	else											        {	
-		printf("and is less than 6 and not 0\n");
+	else if (l == 0)
+        {												        printf("and is 0\n");
+	}
+	else
+	{													printf("and is less than 6 and not 0\n");
 	}
         return (0);
 }
