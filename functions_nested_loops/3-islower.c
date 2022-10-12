cb36d70ab31ea21cd4aaef16ae7730b14ae53ba2
ANDRES-LAW <lawilder.03@gmail.com>
2022-10-12 14:13:52 -0700

    s

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+/**
+ * _islower - checks for lowercase character.
+ * @c: To prove
+ * Return: 1 if lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+	        return (0);									}
+}
