490f02b70e0a684a1179b9ca65c47df95da05712
ANDRES-LAW <lawilder.03@gmail.com>
2022-10-13 15:30:39 -0700

    s

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,7 +8,6 @@
 void jack_bauer(void)
 {
 	int i, j;
-	
 	i = 0;
 
 	while (i < 24)
@@ -16,13 +15,14 @@ void jack_bauer(void)
 		j = 0;
 		while (j < 60)
 		{
-			_putchar((i / 10) + '0');							
+			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');										   _putchar('\n');		        
-			j++;											           }
-		i++;												   
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
 	}
-
 }
