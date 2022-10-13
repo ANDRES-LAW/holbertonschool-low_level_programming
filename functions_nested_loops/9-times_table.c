5c8f7d4a17b3446362e40d1e026cafda01c6c29f
ANDRES-LAW <lawilder.03@gmail.com>
2022-10-13 15:55:46 -0700

    s

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,11 +13,11 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
-
 			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
+
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
@@ -28,9 +28,9 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-			        _putchar((k / 10) + '0');
-			        _putchar((k % 10) + '0');
-		       	}
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
