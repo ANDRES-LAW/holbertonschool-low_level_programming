f21a5016b1fda17ad30129179d95717e9b13da27
ANDRES-LAW <lawilder.03@gmail.com>
2022-10-14 10:48:52 -0700

    s

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,12 @@
 void more_numbers(void)
 {
 	int row, col;
-
-        row = 0;
+	
+	row = 0;
 	for (col = 0; col <= 9; col++)
 	{
 		while (row <= 14)
-          	{
+		{
 			if (row >= 10)
 				_putchar(1 + '0');
 			_putchar((row % 10) + '0');
